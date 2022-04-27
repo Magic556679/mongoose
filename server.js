e5f40316c5040d5067f0e8f72c56dd724fdfdda8
李晏瑋 <magic556679@gmail.com>
2022-04-27 23:45:33 +0800

    feat 修正 404

--- server.js
+++ server.js
@@ -105,11 +105,10 @@ const requestListener = async (req, res) => {
 		res.writeHead(200, headers)
 		res.end();
 	} else {
-		res.writeHead(400, headers)
+		res.writeHead(404, headers)
 		res.write(JSON.stringify({
 			'status': 'false',
 			'message': '欄位輸入錯誤，或無此ID',
-			'error': error
 		}))
 		res.end();
 	}
