c9a821e9f91fe252db2422fd3764d82b031a2ff2
李晏瑋 <magic556679@gmail.com>
2022-05-19 21:49:01 +0800

    feat server.js 拆分到 routes/index.js

---
+++ routes/index.js
@@ -0,0 +1,76 @@
+const Posts = require('../models/posts');
+const headers = require('../headers');
+const handleSuccess = require('../handleSuccess');
+const handleError = require('../handleError');
+
+const routes = async (req, res) => {
+  	let body = "";
+	req.on('data', chunk => {
+		body += chunk
+	});
+	if(req.url === '/posts' && req.method == 'GET'){
+		const posts = await Posts.find();
+		handleSuccess(res, posts);
+	} else if(req.url ==='/posts' && req.method =='POST'){
+		req.on('end', async() => {
+			try {
+				const data = JSON.parse(body)
+				console.log(data)
+				if(data.content){
+					const newPosts = await Posts.create({
+							name: data.name,
+							content: data.content,
+							tags: data.tags,
+							type: data.type
+					});
+					handleSuccess(res, newPosts);
+				} else {
+					handleError(res);
+				}
+			}catch(error){
+				handleError(res, error)
+			}
+		})
+	} else if(req.url ==='/posts' && req.method =='DELETE'){
+		const posts = await Posts.deleteMany({})
+		const allPosts = await Posts.find();
+		handleSuccess(res, allPosts);
+	} else if(req.url.startsWith('/posts/') && req.method =='DELETE'){
+		req.on('end', async() => {
+			try {
+				const id = req.url.split('/').pop();
+				await Posts.findByIdAndDelete(id)
+				const posts = await Posts.find();
+				handleSuccess(res, posts);
+			} catch (error) {
+				handleError(error);
+			}
+		})
+
+	} else if(req.url.startsWith('/posts/') && req.method =='PATCH'){
+		req.on('end', async() => {
+			try {
+				const id = req.url.split('/').pop();
+				const findId = await Posts.find({'_id': id})
+				const data = JSON.parse(body)
+				await Posts.findByIdAndUpdate(findId, {"name": data.name})
+				const posts = await Posts.find({})
+				handleSuccess(res, posts);
+			} catch (error) {
+				handleError(error);
+			}
+		});
+	} else if(req.url ==='/posts' && req.method =='OPTIONS'){
+		res.writeHead(200, headers)
+		res.end();
+	} else {
+		res.writeHead(404, headers)
+		res.write(JSON.stringify({
+			'status': 'false',
+			'message': '欄位輸入錯誤，或無此ID',
+		}))
+		res.end();
+	}
+}
+
+module.exports = routes
